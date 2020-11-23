226eaba57553da1fbd7735f16e1943adcc846673
veronicadvdt <veronicadetoni@gmail.com>
2020-11-23 22:47:56 +0100

    Drawing_assignment_V002

---
+++ public/sketch.js
@@ -0,0 +1,51 @@
+
+let socket = io();
+let myColor = "white";
+
+socket.on("connect", newConnection);
+socket.on("mouseBroadcast", drawOtherMouse);
+socket.on("color", setColor);
+
+
+function setColor(assignedColor) {
+  myColor = assignedColor;
+}
+
+function newConnection() {
+  console.log("your id: " + socket.id);
+}
+
+function drawOtherMouse(data) {
+  push();
+  fill(data.color);
+  ellipse(data.x, data.y, 20);
+  pop();
+}
+
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background("red");
+}
+
+function draw() {
+
+}
+
+function mouseMoved() {
+  push();
+  fill(myColor);
+  ellipse(mouseX, mouseY, 20);
+  pop();
+  // create the message
+  let message = {
+    x: mouseX,
+    y: mouseY,
+    color: myColor,
+  };
+  // send to the server
+  socket.emit("mouse", message);
+}
